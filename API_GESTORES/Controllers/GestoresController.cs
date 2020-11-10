c0833f9028a3b7022e258658f56203603f12a344
David <David0nike@gmail.com>
2020-11-10 00:43:55 -0400

    first commit

---
+++ API_GESTORES/Controllers/GestoresController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_GESTORES.Context;
+using API_GESTORES.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_GESTORES.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestoresController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public GestoresController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.GESTORES_BD.ToList());
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}", Name ="GetGestor")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var gestor = context.GESTORES_BD.FirstOrDefault(g => g.Id == id);
+                return Ok(gestor);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public ActionResult Post([FromBody] GestoresBD gestor)
+        {
+            try
+            {
+                context.GESTORES_BD.Add(gestor);
+                context.SaveChanges();
+                return CreatedAtRoute("GetGestor", new { id = gestor.Id }, gestor);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] GestoresBD gestor)
+        {
+            try
+            {
+                if (gestor.Id == id)
+                {
+                    context.Entry(gestor).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetGestor", new { id = gestor.Id }, gestor);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+                
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var gestor = context.GESTORES_BD.FirstOrDefault(g => g.Id == id);
+                if(gestor != null)
+                {
+                    context.GESTORES_BD.Remove(gestor);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
---
+++ API_GESTORES/Models/GestoresBD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_GESTORES.Models
+{
+    public class GestoresBD
+    {
+
+        [Key]
+        private int _id;
+        private string nombre;
+        private int lanzamiento;
+        private string desarrollador;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Lanzamiento { get => lanzamiento; set => lanzamiento = value; }
+        public string Desarrollador { get => desarrollador; set => desarrollador = value; }
+    }
+}
